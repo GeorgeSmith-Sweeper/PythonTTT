5d95b57f1665febe45298708dadd2deb2a8d217b
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-06 13:24:48 -0400

    boardsize is dynamic, but defaults to 3X3

--- board.py
+++ board.py
@@ -1,7 +1,11 @@
 class Board:
     def __init__(self):
         self.board_state = []
-    '''    
+
     def update_state(self, new_state):
         self.board_state = new_state
-    '''
+    
+    def new_game(self, numRows=3):
+        board_area = numRows*numRows
+        new_board = [" "] * board_area
+        self.update_state(new_board)
--- test_board.py
+++ test_board.py
@@ -4,6 +4,18 @@ from board import Board
 def test_board_state_is_a_list():
     game_board = Board()
     assert game_board.board_state == []
-    '''
-    assert game_board.grid == [" ", " ", " ", " ", " ", " ", " ", " "]
-    '''
+
+def test_when_a_user_begins_a_game_a_3X3_board_is_created():
+    game_board = Board()
+    game_board.new_game()
+    assert game_board.board_state == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def test_when_a_user_supplies_4_rows_a_4x4_board_is_created():
+    game_board = Board()
+    game_board.new_game(4)
+    assert game_board.board_state == [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+'''
+def test_update_state_can_modify_board_state():
+    game_board = Board()
+    game_board.update_state()
+'''
