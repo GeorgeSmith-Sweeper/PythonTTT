dc323b738763f51ce60175ac52d9f501fd0892de
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-06 17:03:11 -0400

    add occupied spot check

--- board.py
+++ board.py
@@ -12,6 +12,12 @@ class Board:
         new_board = [" "] * board_area
         self.board_state = new_board
     
+    def is_occupied(self, current_state, spotChoice):
+        if current_state[spotChoice - 1] != " ":
+            return True
+        else:
+            return False
+
     def display_board(self):
         return self.board_state[0] + " | " + self.board_state[1] + " | " + self.board_state[2] + "\n" +"=========" + "\n" + self.board_state[3] + " | " + self.board_state[4] + " | " + self.board_state[5] + "\n" + "=========" + "\n" + self.board_state[6] + " | " + self.board_state[7] + " | " + self.board_state[8]
 
--- test_board.py
+++ test_board.py
@@ -17,7 +17,7 @@ def test_when_a_user_supplies_4_rows_a_4x4_board_is_created():
 
 # must test 'sad' path for new_game by passing in a value less then 3
 
-def test_when_a_user_picks_a_spot_board_is_displayed_with_their_symbol_at_spot():
+def test_when_a_user_picks_a_spot_board_is_updated_with_their_spot():
     game_board = Board()
     player1 = 'X'
     spot = 1
@@ -25,7 +25,15 @@ def test_when_a_user_picks_a_spot_board_is_displayed_with_their_symbol_at_spot()
     game_board.update_state(game_board.board_state, player1, spot)
     assert game_board.board_state == ["X", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def test_when_a_user_starts_a_game_the_board_is_displayed():
+def test_when_a_user_picks_an_occupied_spot_is_occupied_will_be_True():
+    game_board = Board()
+    player1 = 'X'
+    spot = 1
+    game_board.new_game()
+    game_board.update_state(game_board.board_state, player1, spot)
+    assert game_board.is_occupied(game_board.board_state, spot) == True
+
+def test_current_board_is_displayed():
     game_board = Board()
     game_board.new_game()
     assert game_board.display_board() == game_board.board_state[0] + " | " + game_board.board_state[1] + " | " + game_board.board_state[2] + "\n" +"=========" + "\n" + game_board.board_state[3] + " | " + game_board.board_state[4] + " | " + game_board.board_state[5] + "\n" + "=========" + "\n" + game_board.board_state[6] + " | " + game_board.board_state[7] + " | " + game_board.board_state[8] 
