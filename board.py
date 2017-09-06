fb5fa126ae0d19ef56f9a072bf12afd6c36362f3
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-06 15:33:52 -0400

    test board displays to user on game start

--- board.py
+++ board.py
@@ -10,4 +10,11 @@ class Board:
     def new_game(self, numRows=3):
         board_area = numRows*numRows
         new_board = [" "] * board_area
-        self.board_state = new_board 
+        self.board_state = new_board
+    
+    def display_board(self):
+        return self.board_state[0] + " | " + self.board_state[1] + " | " + self.board_state[2] + "\n" +"=========" + "\n" + self.board_state[3] + " | " + self.board_state[4] + " | " + self.board_state[5] + "\n" + "=========" + "\n" + self.board_state[6] + " | " + self.board_state[7] + " | " + self.board_state[8]
+
+if __name__ == "__main__":
+    game_board = Board()
+    game_board.new_game()
--- test_board.py
+++ test_board.py
@@ -24,3 +24,8 @@ def test_when_a_user_picks_a_spot_board_is_displayed_with_their_symbol_at_spot()
     game_board.new_game()
     game_board.update_state(game_board.board_state, player1, spot)
     assert game_board.board_state == ["X", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def test_when_a_user_starts_a_game_the_board_is_displayed():
+    game_board = Board()
+    game_board.new_game()
+    assert game_board.display_board() == game_board.board_state[0] + " | " + game_board.board_state[1] + " | " + game_board.board_state[2] + "\n" +"=========" + "\n" + game_board.board_state[3] + " | " + game_board.board_state[4] + " | " + game_board.board_state[5] + "\n" + "=========" + "\n" + game_board.board_state[6] + " | " + game_board.board_state[7] + " | " + game_board.board_state[8] 
