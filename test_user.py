551a92f57251f2d321e22fa62f7f6344a03f0ebd
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-07 14:26:09 -0400

    user has a symbol test passes

--- test_user.py
+++ test_user.py
@@ -1,3 +1,7 @@
 import pytest
+from board import Board
+from user import User
 
-
+def test_when_player_is_created_it_has_a_symbol():
+    player1 = User("X")
+    assert player1.symbol == "X" 
---
+++ user.py
@@ -0,0 +1,8 @@
+from board import Board
+
+class User:
+    current_player = ' ' 
+
+    def __init__(self, symbol):
+        self.symbol = symbol
+
