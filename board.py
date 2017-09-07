22fabc2ae9fc5db01dc352c70c56389d08bae201
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-07 11:37:30 -0400

    95% coverage in board.py

--- board.py
+++ board.py
@@ -2,13 +2,13 @@ class Board:
     def __init__(self):
         self.board_state = []
 
-    def update_state(self, current_state, player, spotChoice):
+    def update_state(self, current_state, player, spotchoice):
         new_state = current_state
-        new_state[spotChoice - 1] = player
+        new_state[spotchoice - 1] = player
         self.board_state = new_state
    
-    def new_game(self, numRows=3):
-        board_area = numRows*numRows
+    def new_game(self, numrows=3):
+        board_area = numrows*numrows
         new_board = [" "] * board_area
         self.board_state = new_board
     
@@ -25,4 +25,3 @@ class Board:
             return False
     def display_board(self):
         return self.board_state[0] + " | " + self.board_state[1] + " | " + self.board_state[2] + "\n" +"=========" + "\n" + self.board_state[3] + " | " + self.board_state[4] + " | " + self.board_state[5] + "\n" + "=========" + "\n" + self.board_state[6] + " | " + self.board_state[7] + " | " + self.board_state[8]
-
