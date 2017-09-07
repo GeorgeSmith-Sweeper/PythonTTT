fd478589ffa144270dc99d361d7bf7c929b1ebda
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-07 16:43:47 -0400

    add spot_choice_test

--- test_user.py
+++ test_user.py
@@ -1,7 +1,12 @@
-import pytest
-from board import Board
-from user import User
+from unittest.mock import patch
+from unittest import TestCase
+from user import User, spot_choice
 
 def test_when_player_is_created_it_has_a_symbol():
     player1 = User("X")
     assert player1.symbol == "X" 
+
+class Test(TestCase):
+    @patch('user.get_input', return_value=1)
+    def test_answer_1(self, input):
+        self.assertEqual(spot_choice(), 1) 
--- user.py
+++ user.py
@@ -5,4 +5,16 @@ class User:
 
     def __init__(self, symbol):
         self.symbol = symbol
+    '''    
+    def switch_player(self, player):
+        if self.current_player == player:
+            current_player = player
+        else:
+            return None
+    '''
+def get_input(choice):
+    return input(choice)
 
+def spot_choice():
+    response = int(get_input("Choose number from 1-9: "))
+    return response
