362134a57205d2d791944ad63bd417d7ecb18e91
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-11 07:04:39 -0400

    test "fail path" for spot_choice

--- test_user.py
+++ test_user.py
@@ -1,4 +1,4 @@
-from board import Board
+from board import Board, update_state
 from unittest.mock import patch
 from unittest import TestCase
 from user import User, spot_choice 
@@ -9,14 +9,20 @@ def test_when_player_is_created_it_has_a_symbol():
 
 class TestUserInputs(TestCase):
     @patch('user.get_input', return_value='1')
-    def test_can_receive_user_input(self, input):
+    def test_users_choice_updates_game_board(self, input):
         game_board = Board()
         game_board.new_game()
         self.assertEqual(spot_choice(game_board.board_state), 1)
-'''
+
     @patch('user.get_input', return_value='x')
-    def test_if_users_move_is_not_a_number_return_false(self, input):
+    def test_user_choice_returns_invalid_if_user_inputs_an_invalid_symbol(self, input):
         game_board = Board()
         game_board.new_game()
-        self.assertEqual(spot_choice(), False) 
-'''
+        self.assertEqual(spot_choice(game_board.board_state), 'invalid choice') 
+    
+    @patch('user.get_input', return_value='2')
+    def test_user_choice_returns_invalid_if_user_inputs_an_invalid_symbol(self, input):
+        game_board = Board()
+        game_board.new_game()
+        update_state(game_board.board_state, "X", 2)
+        self.assertEqual(spot_choice(game_board.board_state), 'spot is occupied') 
