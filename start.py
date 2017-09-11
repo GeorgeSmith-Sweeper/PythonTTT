4ab0c77a7330e12a346494955282975ec4b68ef7
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-11 13:48:19 -0400

    game can play to Draw!

--- start.py
+++ start.py
@@ -1,18 +1,26 @@
-from board import Board
+from board import Board, is_draw
 from user import User, spot_choice, turn_swap
 
 def print_board():
     print(game_board.display_board())
 
+def continue_turn_prompt(current_state, player1, player2):
+    if is_draw(current_state):
+        print('DRAW. Game Over')
+        return 'DRAW. Game Over'
+    else: 
+        spot_choice(current_state)
+        print_board()
+        if (User.current_player == player1):
+            turn_swap(User.current_player, player2)
+            continue_turn_prompt(current_state, player1, player2)
+        else: 
+            turn_swap(User.current_player, player1)
+            continue_turn_prompt(current_state, player1, player2)
+        
 if __name__ == "__main__":
     game_board = Board()
     game_board.new_game() 
     player1 = User("X")
     player2 = User("O")
-    print_board()
-    spot_choice(game_board.board_state)
-    turn_swap(User.current_player, player2.symbol)
-    print_board()
-    spot_choice(game_board.board_state)
-    turn_swap(User.current_player, player1.symbol)
-    print_board()
+    continue_turn_prompt(game_board.board_state, player1.symbol, player2.symbol)
--- test_start.py
+++ test_start.py
@@ -1,2 +1,10 @@
+from user import User
+from board import Board
+from start import continue_turn_prompt
 
-
+def test_prompt_game_notifies_user_of_a_draw():
+    game_board = Board()
+    game_board.board_state = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+    player1 = User("X")
+    player2 = User("O")
+    assert continue_turn_prompt(game_board.board_state, player1.symbol, player2.symbol) == 'DRAW. Game Over';
