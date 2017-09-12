54f1d905debdd98cdce1272129ac23d75972b3d9
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-12 10:56:21 -0400

    fix spot_choice tests

--- test_user.py
+++ test_user.py
@@ -7,25 +7,20 @@ def test_when_player_is_created_it_has_a_symbol():
     player1 = User("X")
     assert player1.symbol == "X" 
 
-class TestUserInputs(TestCase):
-    @patch('user.get_input', return_value='1')
-    def test_users_choice_updates_game_board(self, input):
-        game_board = Board()
-        game_board.new_game()
-        self.assertEqual(spot_choice(game_board.board_state), True)
+def test_valid_choice_is_returned():
+    game_board = Board()
+    game_board.new_game()
+    assert spot_choice(game_board.board_state, "1") == 1
     
-    @patch('user.get_input', return_value='x')
-    def test_user_choice_returns_invalid_if_user_inputs_an_invalid_symbol(self, input):
-        game_board = Board()
-        game_board.new_game()
-        self.assertEqual(spot_choice(game_board.board_state), False) 
+def test_invalid_symbol_returns_false():
+    game_board = Board()
+    game_board.new_game()
+    assert spot_choice(game_board.board_state, "x") == False 
     
-    @patch('user.get_input', return_value='2')
-    def test_user_choice_returns_invalid_if_user_inputs_an_invalid_symbol(self, input):
-        game_board = Board()
-        game_board.new_game()
-        update_state(game_board.board_state, "X", 2)
-        self.assertEqual(spot_choice(game_board.board_state), False) 
+def test_invalid_spot_too_large_returns_false():
+    game_board = Board()
+    game_board.new_game()
+    assert spot_choice(game_board.board_state, "12") == False
 
 def test_when_a_user_finishes_move_it_is_the_opponents_turn():
     game_board = Board()
