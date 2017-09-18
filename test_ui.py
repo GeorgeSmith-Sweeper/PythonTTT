2f79c5cce4a6c420a2e3ba9beadc51b4390fa842
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-18 15:02:45 -0400

    attempt test for BoardPresenter

--- test_ui.py
+++ test_ui.py
@@ -1,8 +1,22 @@
 import pytest
-from ui import Ui
+from ui import Ui, BoardPresenter
 from unittest.mock import patch
 from unittest import TestCase
 
 
 def test_ui_display_takes_a_string_and_returns_it():
     assert Ui.msg("Oh nooo") == print("Oh nooo")
+
+'''
+def test_user_is_displayed_a_3x3_grid_with_place_holders():
+    current_board_state = [
+                            "", "", "",
+                            "", "", "",
+                            "", "", "",
+                          ]
+    assert BoardPresenter.display_terminal_board(current_board_state) == '1 | 2 | 3' + '\n' +
+                                                                         '_________' + '\n' +
+                                                                         '4 | 5 | 6' + '\n' +
+                                                                         '_________' + '\n' +
+                                                                         '7 | 8 | 9'
+'''
