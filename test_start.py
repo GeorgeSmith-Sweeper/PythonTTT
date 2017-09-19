cc0431ba4d225eb1749e8c80982b7e9224faeae6
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-19 11:09:57 -0400

    add start_game_win check

--- test_start.py
+++ test_start.py
@@ -12,3 +12,14 @@ def test_prompt_game_notifies_user_of_a_draw():
     player1 = User("X")
     player2 = User("O")
     assert start_game(game_board.board_state, player1.symbol, player2.symbol) == True 
+
+def test_prompt_game_notifies_user_of_a_win():
+    game_board = Board()
+    game_board.board_state = [
+                                "X", "X", "X", 
+                                " ", "X", "O", 
+                                "X", "O", "X"
+                             ]
+    player1 = User("X")
+    player2 = User("O")
+    assert start_game(game_board.board_state, player1.symbol, player2.symbol) == True 
