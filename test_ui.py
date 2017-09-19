7f06cb46c1648551c129eac3538d6e753d135a22
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-19 11:36:57 -0400

    add boardPresenter test

--- test_ui.py
+++ test_ui.py
@@ -7,16 +7,21 @@ from unittest import TestCase
 def test_ui_display_takes_a_string_and_returns_it():
     assert Ui.msg("Oh nooo") == print("Oh nooo")
 
-'''
 def test_user_is_displayed_a_3x3_grid_with_place_holders():
     current_board_state = [
                             "", "", "",
                             "", "", "",
                             "", "", "",
                           ]
-    assert BoardPresenter.display_terminal_board(current_board_state) == '1 | 2 | 3' + '\n' +
-                                                                         '_________' + '\n' +
-                                                                         '4 | 5 | 6' + '\n' +
-                                                                         '_________' + '\n' +
-                                                                         '7 | 8 | 9'
-'''
+    s = ('1 | 2 | 3'
+         '\n'
+         '_________'
+         '\n'
+         '4 | 5 | 6'
+         '\n'
+         '_________'
+         '\n'
+         '7 | 8 | 9'
+         '\n'
+        )
+    assert BoardPresenter.display_terminal_board(current_board_state) == s
