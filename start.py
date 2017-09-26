6768df5a3df0d4fe01f6f5b6a575acc0c543127c
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-26 11:28:07 -0400

    created GameModes class

--- start.py
+++ start.py
@@ -1,82 +1,99 @@
-from board import Board, EndStates, SpotStates, BoardState, WinningCombos 
-from user import User, UserActions 
+from board import Board, EndStates, SpotStates, BoardState, WinningCombos
+from user import User, UserActions
 from ai import Ai
 import board
 from ui import Ui, BoardPresenter, CommandLinePrompt
 
-def begin_game(mode_choosen, current_state, player1, player2):
-    if mode_choosen == "1":
-        human_v_human(current_state, player1, player2)
-    if mode_choosen == "2":
-        human_v_computer(current_state, player1, player2)
-    if mode_choosen == "3":
-        computer_v_computer()
+class GameModes:
+    def human_v_computer(current_state, player1, player2):
+        game_over = False
 
-def human_v_computer(current_state, player1, player2):
-    game_over = False
+        while game_over is False:
+            if User.current_player == player2:
+                Ui.msg("Computer has moved")
+                computers_move = Ai.make_move(current_state)
+                BoardState.update_state(current_state, User.current_player, computers_move)
+                if User.current_player == player1:
+                    User.switch_current_user(User.current_player, player2)
+                User.switch_current_user(User.current_player, player1)
+            else:
+                response = False
+                while response is False:
+                    Ui.msg(BoardPresenter.display_terminal_board(current_state))
+                    response = CommandLinePrompt.get_input("Enter a number from 1-9: ")
+                    response = UserActions.make_move(current_state, response)
+                BoardState.update_state(current_state, User.current_player, response)
 
-    while game_over is False:
-        if User.current_player == player2:
-            Ui.msg("Computer has moved")
-            computers_move = Ai.make_move(current_state)
-            BoardState.update_state(current_state, User.current_player, computers_move)
-            if User.current_player == player1:
-                User.switch_current_user(User.current_player, player2)
-            User.switch_current_user(User.current_player, player1)
-        else:
+                if EndStates.did_a_player_win(current_state, User.current_player, win_config.winning_combos):
+                    Ui.msg(BoardPresenter.display_terminal_board(current_state))
+                    Ui.msg('Game Over: ' + User.current_player + ' WINS!')
+                    return True
+
+                if EndStates.is_draw(current_state):
+                    Ui.msg('DRAW. GameOver')
+                    return True
+
+                if User.current_player == player1:
+                    User.switch_current_user(User.current_player, player2)
+                else:
+                    User.switch_current_user(User.current_player, player1)
+
+    def computer_v_computer(current_state, player1, player2):
+        game_over = False
+        while game_over is False:
+                Ui.msg(BoardPresenter.display_terminal_board(current_state))
+                computers_move = Ai.make_move(current_state)
+                BoardState.update_state(current_state, User.current_player, computers_move)
+                if EndStates.did_a_player_win(current_state, User.current_player, win_config.winning_combos):
+                    Ui.msg(BoardPresenter.display_terminal_board(current_state))
+                    Ui.msg('Game Over: ' + User.current_player + ' WINS!')
+                    return True
+
+                if EndStates.is_draw(current_state):
+                    Ui.msg('DRAW. GameOver')
+                    return True
+
+                if User.current_player == player1:
+                    User.switch_current_user(User.current_player, player2)
+                else:
+                    User.switch_current_user(User.current_player, player1)
+
+
+    def human_v_human(current_state, player1, player2):
+        game_over = False
+        while game_over is False:
             response = False
             while response is False:
                 Ui.msg(BoardPresenter.display_terminal_board(current_state))
-                response = CommandLinePrompt.get_input("Enter a number from 1-9: ") 
+                response = CommandLinePrompt.get_input("Enter a number from 1-9: ")
                 response = UserActions.make_move(current_state, response)
             BoardState.update_state(current_state, User.current_player, response)
-        
+
             if EndStates.did_a_player_win(current_state, User.current_player, win_config.winning_combos):
-                Ui.msg(BoardPresenter.display_terminal_board(current_state)) 
+                Ui.msg(BoardPresenter.display_terminal_board(current_state))
                 Ui.msg('Game Over: ' + User.current_player + ' WINS!')
                 return True
 
             if EndStates.is_draw(current_state):
                 Ui.msg('DRAW. GameOver')
                 return True
-        
+
             if User.current_player == player1:
                 User.switch_current_user(User.current_player, player2)
             else:
                 User.switch_current_user(User.current_player, player1)
 
-def computer_v_computer():
-    Ui.msg("you can get here but nothing happens")
-    return None
+def begin_game(mode_choosen, current_state, player1, player2):
+    if mode_choosen == "1":
+        GameModes.human_v_human(current_state, player1, player2)
+    if mode_choosen == "2":
+        GameModes.human_v_computer(current_state, player1, player2)
+    if mode_choosen == "3":
+        GameModes.computer_v_computer(current_state, player1, player2)
 
-
-def human_v_human(current_state, player1, player2):
-    game_over = False
-    while game_over is False:
-        response = False
-        while response is False:
-            Ui.msg(BoardPresenter.display_terminal_board(current_state))
-            response = CommandLinePrompt.get_input("Enter a number from 1-9: ") 
-            response = UserActions.make_move(current_state, response)
-        BoardState.update_state(current_state, User.current_player, response)
-        
-        if EndStates.did_a_player_win(current_state, User.current_player, win_config.winning_combos):
-            Ui.msg(BoardPresenter.display_terminal_board(current_state)) 
-            Ui.msg('Game Over: ' + User.current_player + ' WINS!')
-            return True
-
-        if EndStates.is_draw(current_state):
-            Ui.msg('DRAW. GameOver')
-            return True
-        
-        if User.current_player == player1:
-            User.switch_current_user(User.current_player, player2)
-        else:
-            User.switch_current_user(User.current_player, player1)
-        
 if __name__ == "__main__":
     game_board = Board()
-    game_board.new_game(3) 
+    game_board.new_game(3)
     win_config = WinningCombos(3)
     win_config.create_winning_combos()
     player1 = User("X")
