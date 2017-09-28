796b30b72047dbc258ce4a8213fb40248e72fc04
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-28 09:48:33 -0400

    can prompt user for a game mode

--- test_game_config.py
+++ test_game_config.py
@@ -5,11 +5,6 @@ from game_config import GameModePrompt, ModeValidation, GameConfig
 import ui
 
 class TestUserModeSelection(TestCase):
-    @patch('ui.CommandLinePrompt.get_input', return_value='1')
-    def test_a_users_selection_is_returned(self, input):
-        game_mode_config = GameModePrompt()
-        self.assertEqual(game_mode_config.ask_user_for_mode(), '1')
-
     @patch('game_config.GameModePrompt.ask_user_for_mode', return_value='1')
     def test_a_valid_mode_is_stored(self, input):
         mode_choice = ModeValidation("1")
